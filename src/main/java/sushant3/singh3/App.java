67a51fcc226ff0cef504e8c1dda5e662f6138fdb
aujanand <anand.anuj91@gmail.com>
2018-07-18 15:51:35 +0530

    def

---
+++ src/main/java/sushant3/singh3/App.java
@@ -0,0 +1,13 @@
+package sushant3.singh3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
